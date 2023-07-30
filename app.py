4fa00c881de13a040a6b00ef3b8dc511a5f3043d
Aprendiz_de_mago <iniciativas.ecb@gmail.com>
2023-07-30 11:28:35 +0200

    commit7

--- app.py
+++ app.py
@@ -1,12 +1,25 @@
 from flask import Flask,jsonify
 import json
+from rivescript import RiveScript
+from fuzzywuzzy import fuzz
 
 
 app=Flask(__name__)
 
+with open('myjson.json', 'r', encoding='utf-8') as f:
+    chatbot_data = json.load(f)
+bot=RiveScript()    
+bot.load_file('eduardo.rivescript')
+bot.sort_replies()
+
+@app.route("/")
+def saludo():
+    return jsonify({"version 1.1":"chatbot Cv-edu"})
+
 @app.route("/chatbot/<string:pregunta>",methods=['GET'])
 def chatbot(pregunta):
-    respuesta="lo que encuentre en la busqueda"
+    user_input=str(pregunta)
+    respuesta=bot.reply("localuser",user_input)
     return jsonify({"respuesta":respuesta})
 
 
