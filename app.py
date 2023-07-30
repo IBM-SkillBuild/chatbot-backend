cf7f4067ef4da6d53cae41759506b3bb827c4f08
Aprendiz_de_mago <iniciativas.ecb@gmail.com>
2023-07-30 12:25:13 +0200

    commit8

--- app.py
+++ app.py
@@ -1,10 +1,13 @@
 from flask import Flask,jsonify
+from flask_cors import CORS, cross_origin
 import json
 from rivescript import RiveScript
 from fuzzywuzzy import fuzz
 
 
 app=Flask(__name__)
+cors = CORS(app)
+app.config['CORS_HEADERS'] = 'Content-Type'
 
 with open('myjson.json', 'r', encoding='utf-8') as f:
     chatbot_data = json.load(f)
@@ -17,6 +20,7 @@ def saludo():
     return jsonify({"version 1.1":"chatbot Cv-edu"})
 
 @app.route("/chatbot/<string:pregunta>",methods=['GET'])
+@cross_origin()
 def chatbot(pregunta):
     user_input=str(pregunta)
     respuesta=bot.reply("localuser",user_input)
