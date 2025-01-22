fa11e879a2a66690149f2bfca665446213dbeb32
IBM-SkillBuild <Api.Nasa.2022@gmail.com>
2025-01-22 11:25:43 +0100

    rutas fotos

--- app.py
+++ app.py
@@ -1,9 +1,11 @@
-from flask import Flask,jsonify
+from flask import Flask,jsonify,send_file
 from flask_cors import CORS, cross_origin
 import json
 from rivescript import RiveScript
 from fuzzywuzzy import fuzz
-
+import requests
+from io import BytesIO
+import random,os
 
 
 app=Flask(__name__)
@@ -16,6 +18,46 @@ app.config['CORS_HEADERS'] = 'Content-Type'
 @app.route("/")
 def saludo():
     return jsonify({"version 1.1":"chatbot Cv-edu"})
+
+@app.route('/foto-mujer')
+def get_foto_female():
+    # URL de la API
+    num_foto = random.randint(1, 99)
+    url = f'https://randomuser.me/api/portraits/med/women/{num_foto}.jpg' #
+   
+    # Hacer la solicitud GET
+    response = requests.get(url)
+
+    # Verificar que la solicitud fue exitosa
+    if response.status_code == 200:
+        # Crear un archivo en memoria
+        img = BytesIO(response.content)
+        img.seek(0)
+        return send_file(img, mimetype='image/jpeg')
+    else:
+        filepath = os.path.join(os.getcwd(), 'foto-mujer.jpeg')
+        return send_file(filepath, mimetype='image/jpeg')
+
+@app.route('/foto-hombre')
+def get_foto_male():
+    # URL de la API
+    num_foto = random.randint(1, 100)
+    url = f'https://randomuser.me/api/portraits/med/men/{num_foto}.jpg' #
+   
+    # Hacer la solicitud GET
+    response = requests.get(url)
+
+    # Verificar que la solicitud fue exitosa
+    if response.status_code == 200:
+        # Crear un archivo en memoria
+        img = BytesIO(response.content)
+        img.seek(0)
+        return send_file(img, mimetype='image/jpeg')
+    else:
+       filepath = os.path.join(os.getcwd(), 'foto-hombre.jpeg')
+       return send_file(filepath, mimetype='image/jpeg')   
+
+
 
 @app.route("/chatbot/<string:pregunta>",methods=['GET'])
 @cross_origin()
